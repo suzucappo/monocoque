317e738d3a79a5c840d3668e088b411c81a485d9
Morgan Garcia <morgan.garcia@skizzles.org>
2025-06-20 16:28:26 -0700

    Update moza_wheel_detect.h

--- src/monocoque/devices/moza_wheel_detect.h
+++ src/monocoque/devices/moza_wheel_detect.h
@@ -1,13 +1,13 @@
 #ifndef MOZA_WHEEL_DETECT_H
 #define MOZA_WHEEL_DETECT_H
 
+#include "../serialadapter.h"
+
 #ifdef __cplusplus
 extern "C" {
     #endif
 
-    // Returns device ID (e.g., 0x13, 0x15, 0x17) or -1 on failure
-    // Pass `verbose = 1` to enable debug output
-    int detect_moza_wheel_id(int verbose);
+    int detect_moza_wheel_id(SerialDevice* serialdevice, int verbose);
 
     #ifdef __cplusplus
 }
